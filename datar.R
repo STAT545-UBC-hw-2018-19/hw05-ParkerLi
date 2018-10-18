bc8575620d86e6226f2012e96c4c0e5643cb771a
Parker Li <parker.lyz@gmail.com>
2018-10-17 23:04:28 -0700

    Add files via upload

---
+++ datar.R
@@ -0,0 +1,5 @@
+structure(list(continent = structure(c(1L, 3L, 5L, 4L, 2L), .Label = c("Africa", 
+"Oceania", "Americas", "Europe", "Asia"), class = "factor"), 
+    maxgdpPecap = c(21951.21176, 42951.65309, 113523.1329, 49357.19017, 
+    34435.36744)), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, 
+-5L))
